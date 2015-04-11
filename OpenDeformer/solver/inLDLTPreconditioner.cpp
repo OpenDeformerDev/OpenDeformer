634e65c956c2ce101af673c763cef17bf800e937
buyaoshuo <buyaoshuo1992@gmail.com>
2015-04-11 15:03:57 +0800

    incLDLT tol

--- OpenDeformer/solver/inLDLTPreconditioner.cpp
+++ OpenDeformer/solver/inLDLTPreconditioner.cpp
@@ -37,6 +37,9 @@ namespace ODER{
 		for (int i = 0; i < columnCount; i++)
 			vecs[i].emplaceBack(i, 1.0);
 
+		SparseVector test, ret;
+		test.emplaceBack(6, 1.0);
+		SpMSV(mat, test, ret);
 		int count = 0;
 		for (int i = 0; i < columnCount - 1; i++){
 			SpMSV(mat, vecs[i], temp);
